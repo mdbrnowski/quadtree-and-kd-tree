259e5681b9c0680d3af997eb31089324504e5ccc
Wiktor <wiktor.prczk@gmail.com>
2023-12-29 18:05:47 +0100

    Add tests to kd_tree and kd_tree_visualization

---
+++ tests/test_kd_tree_visualization.py
@@ -0,0 +1,18 @@
+import unittest
+from geometry import Rectangle
+from kd_tree_visualization import KdTreeVis
+
+
+class QuadtreeVisualizationTest(unittest.TestCase):
+    def test_visualization(self):
+        points = [(0, 0), (0, 1), (1, 1), (1, 0)]
+        rectangle = Rectangle(0, 0.2, 0.8, 1)
+        try:
+            tree = KdTreeVis(points)
+            tree.find(rectangle)
+        except Exception as e:
+            self.fail(f'Quadtree visualization raised "{e}"')
+
+
+if __name__ == '__main__':
+    unittest.main()
